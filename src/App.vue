86ccf66f4c9b7202a57c5e152ca2eede54c9895b
tiico <ockelberg@gmail.com>
2019-08-23 14:28:36 +0200

    nuråå

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to the test App jobba dååååå"/>
+    <HelloWorld msg="Welcome to the test App jobba dååååååååå"/>
   </div>
 </template>
 
