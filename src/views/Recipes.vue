c29f266b71ef0410b7359a31b1656c989cd63a06
tiico <ockelberg@gmail.com>
2019-09-25 14:51:22 +0200

    test

--- src/views/Recipes.vue
+++ src/views/Recipes.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="recipes">
     <h1>This is a recipe page</h1>
-    <Recipe></Recipe>
+    <!-- <Recipe></Recipe> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Recipe from '@/components/RecipeComponent.vue'
+// import Recipe from '@/components/RecipeComponent.vue'
 
 export default {
   name: 'recipes',
   components: {
-    Recipe
+    // Recipe
   }
 }
 </script>
